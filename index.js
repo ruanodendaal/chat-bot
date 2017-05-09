fea84e06d4a491f1b0a87e769070c46d27406d10
Ruan Odendaal <ruanodendaal@gmail.com>
2017-05-09 08:47:40 +0100

    updated bot to speak

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -28,3 +30,40 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+
+var token = process.env.APP_SECRET
